2f370c12f233ebe0cc4a635740c3b759c8e2da46
Eric Seelig <esseelig@gmail.com>
2023-10-09 20:23:13 -0500

    Completed Class 9 Studio: Unit Testing

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,7 +1,60 @@
 
 let launchcode = {
-  
+  organization: "nonprofit",
+  executiveDirector: "Jeff",
+  percentageCoolEmployees: 100,
+  programsOffered: ["Web Development", "Data Analysis", "Liftoff"],
+  launchOutput: function(number) {
+    outputText = ""
+    if (number % 2 === 0) {
+        outputText = outputText + "Launch"
+    }
+    if (number % 3 === 0) {
+        outputText = outputText + "Code"
+    }
+    if (number % 5 === 0) {
+        if (outputText === "") {
+            outputText = outputText + "Rocks"
+        } else {
+        outputText = outputText + " Rocks"
+        }
+    }
+
+    if (number % 2 !== 0 && number % 3 !== 0 && number % 5 !== 0) {
+        return "Rutabagas! That doesn't work."
+    }
+
+    if (number % 2 === 0 && number % 5 === 0 && number % 3 !== 0 ) {
+        return (outputText + "! (CRASH!!!!)")
+    } else {return (outputText + "!")
+    }
+
+    }
 }
 
+
 module.exports = launchcode;
+
+//   Here is what they need the launchcode object to contain:
+
+// A key called organization with a value of "nonprofit".
+// A key called executiveDirector with a value of "Jeff".
+// A key called percentageCoolEmployees with a value of 100.
+// A key called programsOffered with a value of ["Web Development", "Data Analysis", "Liftoff"].
+// And a method called launchOutput(). This method will return a string.
+
+// launchOutput() needs to meet the following conditions:
+
+// When passed a number that is ONLY divisible by 2, return 'Launch!'
+// When passed a number that is ONLY divisible by 3, return 'Code!'
+// When passed a number that is ONLY divisible by 5, return 'Rocks!'
+// When passed a number that is divisible by 2 AND 3, return 'LaunchCode!'
+// When passed a number that is divisible by 3 AND 5, return 'Code Rocks!'
+// When passed a number that is divisible by 2 AND 5, return 'Launch Rocks!'
+// When passed a number that is divisible by 2, 3, AND 5, return 'LaunchCode Rocks!'
+// When passed a number that is NOT divisible by 2, 3, or 5, return 'Rutabagas! That doesn't work.'
+// To make sure that you meet all of these conditions, you need to take it one test at a time.
+
+
+
 
--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -4,5 +4,86 @@ const launchcode = require('../index.js');
 describe("Testing launchcode", function(){
 
   // Write your unit tests here!
+  // A key called organization with a value of "nonprofit".
+  test("organization should have value of 'nonprofit'", () => {
+    expect(launchcode.organization).toEqual("nonprofit");
+  });
+
+  // A key called executiveDirector with a value of "Jeff".
+  test("executiveDirector should have value of 'Jeff'", () => {
+    expect(launchcode.executiveDirector).toEqual("Jeff");
+  });
+
+// A key called percentageCoolEmployees with a value of 100.
+test("percentageCoolEmployees should have value of 100", () => {
+  expect(launchcode.percentageCoolEmployees).toEqual(100);
+});
+
+// A key called programsOffered with a value of ["Web Development", "Data Analysis", "Liftoff"].
+test("programsOffered should have length of 3 and proper values", () => {
+  expect(launchcode.programsOffered).toContain("Web Development");
+  expect(launchcode.programsOffered).toContain("Data Analysis");
+  expect(launchcode.programsOffered).toContain("Liftoff");
+  expect(launchcode.programsOffered.length).toEqual(3);
+});
+
+// And a method called launchOutput(). This method will return a string.
+
+// launchOutput() needs to meet the following conditions:
+
+// When passed a number that is ONLY divisible by 2, return 'Launch!'
+
+test("When passed a number that is ONLY divisible by 2, launchOutput() returns 'Launch!'", () => {
+  expect(launchcode.launchOutput(2)).toEqual('Launch!');
+});
+
+// When passed a number that is ONLY divisible by 3, return 'Code!'
+
+test("When passed a number that is ONLY divisible by 3, launchOutput() returns 'Code!'", () => {
+  expect(launchcode.launchOutput(3)).toEqual('Code!');
+});
+
+// When passed a number that is ONLY divisible by 5, return 'Rocks!'
+
+test("When passed a number that is ONLY divisible by 5, launchOutput() returns 'Rocks!'", () => {
+  expect(launchcode.launchOutput(5)).toEqual('Rocks!');
+});
+
+// When passed a number that is divisible by 2 AND 3, return 'LaunchCode!'
+
+test("When passed a number that is divisible by 2 AND 3, return 'LaunchCode!'", () => {
+  expect(launchcode.launchOutput(6)).toEqual('LaunchCode!');
+});
+
+// When passed a number that is divisible by 3 AND 5, return 'Code Rocks!'
+
+test("When passed a number that is divisible by 3 AND 5, return 'Code Rocks!'", () => {
+  expect(launchcode.launchOutput(15)).toEqual('Code Rocks!');
+});
+
+// When passed a number that is divisible by 2 AND 5, return 'Launch Rocks!'
+
+test("When passed a number that is divisible by 2 AND 5, return 'Launch Rocks!'", () => {
+  expect(launchcode.launchOutput(10)).toEqual('Launch Rocks! (CRASH!!!!)');
+});
+
+// When passed a number that is divisible by 2, 3, AND 5, return 'LaunchCode Rocks!'
+
+test("When passed a number that is divisible by 2, 3, AND 5, return 'LaunchCode Rocks!'", () => {
+  expect(launchcode.launchOutput(30)).toEqual('LaunchCode Rocks!');
+});
+
+// When passed a number that is NOT divisible by 2, 3, or 5, return 'Rutabagas! That doesn't work.'
+
+test("When passed a number that is NOT divisible by 2, 3, or 5, return 'Rutabagas! That doesn't work.'", () => {
+  expect(launchcode.launchOutput(7)).toEqual("Rutabagas! That doesn't work.");
+});
+
+
+// To make sure that you meet all of these conditions, you need to take it one test at a time.
+
+
+
+
   
 });
